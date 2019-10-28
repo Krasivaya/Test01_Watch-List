01df7c962348d7d61d348988bdd008c6c0c452bd
Krasivaya <semwagacarine@gmail.com>
2019-10-28 09:58:26 +0200

    Start test01

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunincorn manage:app
--- app/main/error.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from . import main
-
-#Error view
-@main.app_errorhandler(404)
-def four_ow_four(error):
-    return render_template('fourOwfour.html'),404
